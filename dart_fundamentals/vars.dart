50658354fa29baa3feafefe909ebca061d86de18
nomangul <nomangul2001@gmail.com>
2022-01-08 21:29:13 +0500

    :sparkles: feat: practive dart variables, types and main function

---
+++ dart_fundamentals/vars.dart
@@ -0,0 +1,21 @@
+void main() {
+  // here is the story
+  String name = 'Chai wala';
+  int age = 55;
+  double height = 2.9;
+  List<String> ingredients = ['patti', 'milk', 'sugar'];
+  print("Hello, I'm ${name}");
+  print("from the city of lights Karachi, Pakistan");
+  print("He is ${age} years old and ${height} feet tall");
+  print("He is a very good tea maker");
+  print("He has a temporary memory - just like a RAM");
+  print("so let's store tea count in a variable");
+  var teaCount = 1 * 6;
+  print("He will be making $teaCount cups tea for us using $ingredients");
+
+  // this is a comment
+
+  /**
+   * this is a multiline comment
+   */
+}
