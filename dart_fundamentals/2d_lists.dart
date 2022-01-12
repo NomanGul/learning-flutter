7c2654a17c04f7bcfa29ff2ce30bc0c1b3bce816
nomangul <nomangul2001@gmail.com>
2022-01-12 21:00:00 +0500

    :sparkles: add third assignment - 2d list

---
+++ dart_fundamentals/2d_lists.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<List<String>> users = [
+    ["name", "age", "email"],
+    ["Muhammad", "40", "muhammad@gmail.com"],
+    ["Ahmad", "23", "ahmad@gmail.com"],
+    ["Ali", "25", "ali@gmail.com"],
+  ];
+  print("users -> $users");
+}
